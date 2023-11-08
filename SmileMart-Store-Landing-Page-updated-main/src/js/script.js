26296010195a4dafd4a705800207eb1ee6f82ee8
Melvar1 <126205236+Melvar1@users.noreply.github.com>
2023-11-08 11:23:43 -0600

    Add files via upload

---
+++ SmileMart-Store-Landing-Page-updated-main/src/js/script.js
@@ -0,0 +1,25 @@
+function customOpenForm() {
+  document.getElementById("customMyForm").style.display = "block";
+}
+
+function customCloseForm() {
+  document.getElementById("customMyForm").style.display = "none";
+}
+
+async function setIframeSrc() {
+  const response = await fetch('https://demo.redwater-75eedb1d.eastus.azurecontainerapps.io/get-chatbot-token',{
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+  const json = await response.json();
+
+  document.getElementById("chatbot-iframe").src = "https://webchat.botframework.com/embed/smileMartBot-bot?s=" + json.key;
+}
+
+setIframeSrc()
+
+function customOpenForm() {
+  document.getElementById("customMyForm").style.display = "block";
+  document.getElementById("custom-open-button").style.display = "none";
+}
